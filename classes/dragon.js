6a7febb3cf46cbb9de911e7612a1cecfd8aa59de
lanre <islukmon@gmail.com>
2024-08-08 20:02:57 +0100

    dragon made by ynglukas

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,20 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name)
+  }
+}
+
+module.exports = Dragon;
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,35 @@
 // Your code here
+const Dragon = require('./dragon')
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals = [], friend) {
+        super(name, color)
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach(lifeGoal => {
+            console.log(`${this.name} likes to ${lifeGoal}`)
+        });
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+
+module.exports = FriendlyDragon;
